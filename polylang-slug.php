797370f76149276e429d216cccb30a7c35d5dd6f
Ulrich Pogson <grapplerulrich@gmail.com>
2015-03-13 10:02:32 +0100

    Move code to single file
    - Add check for WordPress 3.9

--- polylang-slug.php
+++ polylang-slug.php
@@ -29,11 +29,9 @@
 // Built using code from: https://wordpress.org/support/topic/plugin-polylang-identical-page-names-in-different-languages?replies=8#post-2669927
 
 // Check if Polylang_Base exists and if $polylang is the right object
-if ( ! class_exists( 'PLL_Model' ) ) {
+if ( ! class_exists( 'PLL_Model' && version_compare( $GLOBALS[ 'wp_version' ], '3.9', '>=' ) ) {
 	return;
 }
-
-require dirname( __FILE__ ) . '/query.php';
 
 /**
  * Checks if the slug is unique within language.
@@ -104,3 +102,32 @@ function polylang_slug_unique_slug_in_language( $slug, $post_ID, $post_status, $
 
 }
 add_filter( 'wp_unique_post_slug', 'polylang_slug_unique_slug_in_language', 10, 6 );
+
+// https://developer.wordpress.org/reference/hooks/query/
+function polylang_slug_filter_queries( $sql ) {
+	global $wpdb, $polylang;
+	// keep a record of the queries
+	$queries[] = $sql;
+
+	$is_main_sql = preg_match( "#\n\t\tSELECT ID, post_name, post_parent, post_type\n\t\tFROM {$wpdb->posts}\n\t\tWHERE post_name IN \(([^)]+)\)\n\t\tAND post_type IN \(([^)]+)\)#", $sql, $matches );
+
+	if ( $is_main_sql ) {
+
+		$lang = pll_current_language();
+
+		// " INNER JOIN $wpdb->term_relationships AS pll_tr ON pll_tr.object_id = " . ('term' == $type ? "t.term_id" : "ID");
+		$join_clause  = $polylang->model->join_clause( 'post' );
+		// " AND pll_tr.term_taxonomy_id IN (" . implode(',', $languages) . ")"
+		$where_clause = $polylang->model->where_clause( $lang, 'post' );
+
+		$sql = "SELECT ID, post_name, post_parent, post_type
+				FROM {$wpdb->posts}
+				$join_clause
+				WHERE post_name IN ({$matches[1]})
+				AND post_type IN ({$matches[2]})
+				$where_clause";
+	}
+
+	return $sql;
+}
+add_filter( 'query', 'polylang_slug_filter_queries' );
